1e119d5666d4e484efeeffe4ee9b0eb0907bd896
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-20 16:35:00 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -18,6 +18,14 @@ elif option=='map':
     np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
     columns=['lat', 'lon'])
     st.map(map_data)
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+          'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+          'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        }))
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
