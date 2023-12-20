c5fb0fd72fc2357634dc3ba86692e1c20320615f
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-20 16:23:03 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -6,9 +6,9 @@ st.header("My first Streamlit App")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
-st.write(pd.DataFrame({
+
 if show:
     st.write(pd.DataFrame({
-    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-    'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+        'Attendance Status': ['yes', 'yes', 'yes', 'no']
     }))
