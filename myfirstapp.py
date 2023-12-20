d62411d129d2fcb70b3880e9bd4ec57f283c379c
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-20 16:27:31 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 st.header("My first Streamlit App")
 
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C'])
+
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
 
