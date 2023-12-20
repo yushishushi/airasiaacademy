9ea4d9c6bc76a4f2e77a9c075bf492ffebf57ee4
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-20 16:37:49 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -26,12 +26,3 @@ elif option=='T n C':
           'Students': ['John', 'Lofa', 'Siti', 'Amy'],
           'Attendance Status': ['yes', 'yes', 'yes', 'no']
         }))
-
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-
-if show:
-    st.write(pd.DataFrame({
-        'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-        'Attendance Status': ['yes', 'yes', 'yes', 'no']
-    }))
