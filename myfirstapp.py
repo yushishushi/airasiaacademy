dd33c868a122aa30e743eae5575ea4185164bb46
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-20 16:30:42 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -8,6 +8,12 @@ option = st.sidebar.selectbox(
     'Select a mini project',
      ['line chart','map','T n C'])
 
+if option=='line chart':
+    chart_data = pd.DataFrame(
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
+
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
 
