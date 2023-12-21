cd4e02bb88dad8e3a60735a132970168cb1344b5
yushishushi <154412113+yushishushi@users.noreply.github.com>
2023-12-21 14:09:37 +0800

    Update iris-generate-predict-project.py

--- iris-generate-predict-project.py
+++ iris-generate-predict-project.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import pickle 
 
 
 st.write("# Simple Iris Flower Prediction App")
@@ -25,9 +26,6 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df) 
 
-
-modelsvmc = SVC() 
-modelsvmc.fit(X, Y)
 
 prediction = modelsvmcIris.predict(df) 
 prediction_proba = modelsvmcIris.predict_proba(df)
